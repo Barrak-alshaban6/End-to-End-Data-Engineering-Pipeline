384cf6ab9344ecb18e94d850efc00783364aa3b6
barrak alshaban <barrakammar0@gmail.com>
2025-09-22 08:15:28 +0300

    Delete src/piplines/real-estate/solids.py

--- src/piplines/real-estate/solids.py
+++
@@ -1,9 +0,0 @@
-from dagster import op, Optional, Bool, Out
-
-
-@op(
-    out={"foo_output": Out(Optional[float], is_required=False)},
-)
-def condition_check_bool(condition: Bool):
-    if condition:
-        yield 1.0
